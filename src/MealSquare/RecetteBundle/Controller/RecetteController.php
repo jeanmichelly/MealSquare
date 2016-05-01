498779ea69524cd1a99dea39ee43f80b76a22150
Jean-Michel <jeanmichely@gmail.com>
2016-05-01 12:54:12 +0200

    Mettre en place un système de commentaire

--- src/MealSquare/RecetteBundle/Controller/RecetteController.php
+++ src/MealSquare/RecetteBundle/Controller/RecetteController.php
@@ -13,6 +13,7 @@ use MealSquare\RecetteBundle\Form\RecetteEditType;
 use Doctrine\Common\Util\Debug;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use FOS\UserBundle\Model\UserInterface;
+use Application\Sonata\NewsBundle\Entity\Post;
 
 class RecetteController extends Controller {
 
@@ -430,6 +431,45 @@ class RecetteController extends Controller {
 
             return $this->render('MealSquareRecetteBundle:Recette:add.html.twig', array('form' => $form->createView(),'isVersion'=>$isVersion));            
         }
+    }
+
+    public function addCommentAction() {
+
+        $em = $this->getDoctrine()->getManager();
+        
+        $repository     = $em->getRepository("MealSquareRecetteBundle:Recette");
+        $recette        = $repository->findOneById($this->container->get('request')->get('idRecette'));
+
+        $user = $this->get('security.context')->getToken()->getUser();
+
+        $comment = new Post();
+
+        $comment->setRecette($recette);
+        $comment->setAuthor($user);
+        $comment->setImage($user->getAvatar());
+        $comment->setTitle("");
+        $comment->setAbstract("");
+        $comment->setContent($this->container->get('request')->get('comment'));
+        $comment->setRawContent("");
+        $comment->setContentFormatter("");
+        $comment->setEnabled(true);
+        $comment->setCommentsDefaultStatus("");
+
+        $recette->addComment($comment);
+
+        $em->persist($comment);
+        $em->persist($recette);
+        $em->flush();  
+
+        $rendered = $this->renderView(
+            'MealSquareRecetteBundle:Recette:comment.html.twig', 
+            array( 'comment' => $comment )
+        );
+        
+        $response = new Response();
+        $response->setContent(json_encode($rendered));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
 }
